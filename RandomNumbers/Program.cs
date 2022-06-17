035b08796a24f0155961137133c2d32564af6df4
Brit <Brit@DESKTOP-BFORP22>
2022-06-16 17:27:01 -0700

    Add project files.

---
+++ RandomNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿Random random = new Random();
+
+int num1 = random.Next(1, 21);
+int num2 = random.Next(1, 21);
+int num3 = random.Next(1, 21);
+
+//double num = random.NextDouble();
+
+Console.WriteLine(num1);
+
+Console.WriteLine(num2);
+
+Console.WriteLine(num3);
+
+Console.ReadKey();
